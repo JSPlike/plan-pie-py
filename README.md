d0b88ff412b109f8a884c8d0aae2317b2cf272a4
JSPLike <moter2588@gmail.com>
2025-03-19 18:27:27 +0900

    fragments

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # plan-pie-py
 plan pie app with django
+
+## 관리자페이지
+
+## 달력페이지
+
+## 일정리스트
+
+## 고객페이지
---
+++ plan_pie/events/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class Customer(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    
+class Event(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    customer = models.ForeignKey(Customer, related_name="events", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
